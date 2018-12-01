b9059ad049018cc3520bb8e2df74307eb3f7004e
thanasis karidis <than.karidis@gmail.com>
2018-12-01 14:55:31 +0200

    add project

---
+++ project/src/main/java/team/KafkaSource.java
@@ -0,0 +1,46 @@
+package team;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+
+public class KafkaSource {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("/home/eleni/BigDataMining/ais_data_small.csv");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName()); // our key and values are String
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        properties.setProperty("group.id", "test");
+
+        Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(properties);
+
+        String line = " ";
+
+        int key = 0;
+        while ((line = br.readLine()) != null) {
+            //  System.out.println(line);
+
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("test", Integer.toString(key), line);
+            key++;
+            System.out.println(key);
+            producer.send(producerRecord);
+            producer.flush();
+
+        }
+        producer.close();
+        System.out.println("exit");
+    }
+}
